c8a86c7e3631f64a3a0181b9b84fde52d96fa775
963838470 <963838470@qq.com>
2018-03-20 14:03:05 +0800

    异常修正

--- letsgo.py
+++ letsgo.py
@@ -9,9 +9,10 @@ IGNORE_DOGS = ['11064331', '17484745']
 
 if __name__ == "__main__":
     browser = webdriver.Chrome()  # 打开浏览器
-    # browser.set_page_load_timeout(10)  # 页面加载时的超时时间
-    # browser.set_script_timeout(10)  # 异步脚本的超时时间
-    browser.implicitly_wait(3)  # 隐性等待时间
+    wait_time = 5
+    browser.implicitly_wait(wait_time)  # 识别元素时的超时时间
+    browser.set_page_load_timeout(wait_time)  # 页面加载时的超时时间
+    browser.set_script_timeout(wait_time)  # 异步脚本的超时时间
 
     # browser.get('https://pet-chain.baidu.com/chain/dogMarket')
     # js = "scrollTo(0,document.body.scrollHeight+10)"
@@ -68,7 +69,7 @@ if __name__ == "__main__":
                         price = 500000
                     img = dog.find_element_by_css_selector('img')
                     img.click()
-                    # time.sleep(1)
+                    time.sleep(1)
 
                     sold_btn = browser.find_element_by_css_selector('.button')
                     sold_btn.click()
